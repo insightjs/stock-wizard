a51d6bae4a106e7c8090e4fcc58c8865d8ec8d3a
eternalee <one.eternity@gmail.com>
2018-12-10 23:45:48 -0500

    react hooks

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,38 +1,37 @@
-// import { useState, useEffect } from 'react';
-// import '../app.css'
+import '../app.css';
+import React, { useState, useEffect } from 'react';
 
-// function App() {
-//   const [count, setCount] = useState(0);
+export default function App() {
+  const [count, setCount] = useState(0);
 
-//   useEffect(() => {
-//     document.title = `You clicked ${count} times`;
-//   });
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
 
-//   return (
-//     <div>
-//       <H1>Hello World!</H1>
-//       <p>You clicked {count} times</p>
-//       <button onClick={() => setCount(count + 1)}>Click</button>
-//     </div>
-//   );
+  return (
+    <div>
+      <h1>You clicked {count} times</h1>
+      <button onClick={() => setCount(count + 1)}>Click</button>
+    </div>
+  );
+}
+
+// import React, { Component } from 'react';
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: [0, 1, 2]
+//     }
+//   }
+
+//   render() {
+//     return(
+//       <div>
+//         <h1>{this.state.data}</h1>
+//       </div>
+//     )
+//   }
 // }
 
-import React, { Component } from 'react';
-import '../app.css';
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [0, 1, 2]
-    }
-  }
-
-  render() {
-    return(
-      <div>
-        <h1>{this.state.data}</h1>
-      </div>
-    )
-  }
-}
